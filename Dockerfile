4f220be048b9012d95e29d86c1949de992674e5c
Alex Ellis <alexellis2@gmail.com>
2017-09-04 16:41:27 +0000

    fast fork, for faster forking
    Signed-off-by: Alex Ellis <alexellis2@gmail.com>

--- Dockerfile
+++ Dockerfile
@@ -4,15 +4,17 @@ RUN apt-get update && apt-get install -qy python3-pip
 WORKDIR /root/
 COPY requirements.txt ./
 RUN pip3 install -r requirements.txt
-
-ADD https://github.com/alexellis/faas/releases/download/0.6.2/fwatchdog /usr/bin
+RUN apt-get install -qy vim nano
+#ADD https://github.com/alexellis/faas/releases/download/0.6.2/fwatchdog /usr/bin
+COPY ./fwatchdog /usr/bin/
 RUN chmod +x /usr/bin/fwatchdog
 
 ENV TF_CPP_MIN_LOG_LEVEL=3
 COPY *.py ./
 RUN python3 ./pre_download.py
 COPY *.txt ./
-
+COPY ./test.raw .
 ENV fprocess="python3 index.py"
+ENV fast_fork=1
 CMD ["fwatchdog"]
 #CMD ["python3", "./index.py"]
--- index.py
+++ index.py
@@ -1,18 +1,51 @@
-# Copyright (c) Alex Ellis 2017. All rights reserved.
-# Licensed under the MIT license. See LICENSE file in the project root for full license information.
-
+from http_parser.pyparser import HttpParser
 import sys
-#from function import handler
 import inception
-def get_stdin():
-    buf = ""
-    for line in sys.stdin:
-        buf = buf + line
+
+def read_body(content_length):
+    return sys.stdin.read(content_length)
+    
+def read_head():
+    buf = None
+    while(True):
+        line = sys.stdin.buffer.readline()
+        if buf == None:
+            buf = line
+        else:
+            buf += line
+
+        if line.decode("utf-8") == "\r\n":
+            break
+        elif line.decode("utf-8") == "" or line == None:
+            break
+
     return buf
 
-if(__name__ == "__main__"):
-    st = get_stdin()
-    #handler.handle(st)
-    print(inception.invoke(st))
+def main():
 
+    p = HttpParser()
+    while True:
+        header = read_head()
+        if header.decode("utf-8") == "":
+            return
+        res = p.execute(header, len(header))
+        result = None
+        length_key = "content-length"
+        content_length = p.get_headers()[length_key]
+        if content_length != None:
+            body = read_body(int(content_length))
+            result = handle(body)
 
+        out_buffer = "HTTP/1.1 200 OK\r\n"
+        out_buffer += "Content-Length: "+str(len(result))+"\r\n"
+        out_buffer += "\r\n"
+        out_buffer += result
+
+        sys.stdout.write(out_buffer)
+        sys.stdout.flush()
+
+def handle(body):
+    return inception.invoke(body)
+
+if __name__ == "__main__":
+    main()
